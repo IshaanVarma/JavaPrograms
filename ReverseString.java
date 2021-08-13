d731b4b3d70d731c1ab90fff2053b2b9af43e990
Ishaan Varma <56663626+IshaanVarma@users.noreply.github.com>
2021-08-13 03:17:37 +0000

    input is nextLine not nextString

--- ReverseString.java
+++ ReverseString.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class ReverseString {
 
     //1. using for loop and charAt
     public static void main(String[] args) 
     {
-        String s="Ishaan";
+
+      Scanner ob = new Scanner(System.in);
+
+      System.out.println("Insert a word or name to be reversed:");
+      String s = ob.nextLine();
         
         String reverse="";
         for(int i=s.length()-1;i>=0;i--)
